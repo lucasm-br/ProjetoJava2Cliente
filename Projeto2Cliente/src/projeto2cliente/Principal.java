b63ac06e8518f0b39f1e24d57671870702bfd549
Lucas Miranda <lucasm0692@gmail.com>
2012-10-12 21:49:19 -0300

    Criação do projeto.

---
+++ Projeto2Cliente/src/projeto2cliente/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto2cliente;
+
+/**
+ *
+ * @author lucas
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
